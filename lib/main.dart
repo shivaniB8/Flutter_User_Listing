d897ee61e17fe45b500ffc7bde67199f365ee087
shivaniB8 <>
2022-04-26 08:20:40 +0530

    API listing for users data.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:api_list/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/home',
+      routes: {'/home': (context) => const HomePage()},
+    );
+  }
+}
